521b94269f8b6d284fdca8e5b5fe8f0aae4ed498
vuphongle <lephong29092909@gmail.com>
2025-04-29 00:27:38 +0700

    Add GitHub Actions for Gradle build

--- src/main/java/vn/edu/iuh/fit/W10CicdWithGithubActionApplication.java
+++ src/main/java/vn/edu/iuh/fit/W10CicdWithGithubActionApplication.java
@@ -8,6 +8,7 @@ public class W10CicdWithGithubActionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(W10CicdWithGithubActionApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
